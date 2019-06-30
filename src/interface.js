d827c2267a5a23c532d5198cea855d939b83d555
Ruth Baker <ruthmoog@gmail.com>
2019-06-30 09:45:57 +0100

    Connect temperature controls with the interface

--- src/interface.js
+++ src/interface.js
@@ -8,15 +8,35 @@ $(document).ready(function() {
     var hi = thermostat.energyUsage();
     alert(`Energy usage is calculated as: ${hi}`);
   });
+  
+  $("#temp_down").click(function() {
+    indicateEnergyUsage();
+    thermostat.down();
+    indicateTemperature();
+  });
+
+  $("#temp_up").click(function() {
+    indicateEnergyUsage();
+    thermostat.up();
+    indicateTemperature();
+  });
+  
+  $("#reset").click(function() {
+    thermostat.reset();
+    indicateTemperature();
+    indicateEnergyUsage();
+
+  });
 
   function indicateEnergyUsage() {
     var status = thermostat.energyUsage();
     $("#energy_use")
-      .removeClass()
-      .addClass(`${status}`);
+    .removeClass()
+    .addClass(`${status}`);
   }
 
   function indicateTemperature() {
     $("h1").text(`${thermostat.temp}`);
   }
+
 });
